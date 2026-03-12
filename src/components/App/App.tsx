29c888a618ff6f9e2b2ef05b62aa4c8d5eb527a6
Johnny <esp25goodlife@gmail.com>
2026-03-12 09:33:03 +0200

    add create form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,28 +1,37 @@
 import { useState } from 'react';
 import { data } from '../../data/data';
 import { StudentsList } from '../StudentsList/StudentsList';
-// import { type StudentType } from '../../types/studentType';
+import { type StudentType } from '../../types/studentType';
+import { CreateStudentForm } from '../CreateStudentForm/CreateStudentForm';
 import './App.css';
 export const App = () => {
   const [students, setStudents] = useState(data);
+  const [isFormVisible, setIsFormVisible] = useState(true);
   const deleteStudent = (id: number) => {
     setStudents(prevStudents => prevStudents.filter(x => x.id !== id));
   };
   const changeStatus = (id: number) => {
-    // const currentStudent: StudentType | undefined = students.find(
-    //   x => x.id === id,
-    // );
-    // currentStudent!.isOnline = !currentStudent!.isOnline;
-    // setStudents([...students, currentStudent!]);
     setStudents(prev =>
       prev.map(st => (st.id === id ? { ...st, isOnline: !st.isOnline } : st)),
     );
   };
+  const formRender = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+  const addStudent = (newStudent: StudentType) => {
+    setStudents([...students, newStudent]);
+  };
   return (
-    <StudentsList
-      studentsList={students}
-      deleteItem={deleteStudent}
-      changeStatus={changeStatus}
-    />
+    <>
+      <StudentsList
+        studentsList={students}
+        deleteItem={deleteStudent}
+        changeStatus={changeStatus}
+      />
+      <button onClick={formRender}>
+        {isFormVisible ? 'Hide form' : 'Add student'}
+      </button>
+      {isFormVisible && <CreateStudentForm onAdd={addStudent} />}
+    </>
   );
 };
--- src/components/CreateStudentForm/CreateStudentForm.tsx
+++ src/components/CreateStudentForm/CreateStudentForm.tsx
@@ -1,11 +1,36 @@
-export const CreateStudentForm = () => {
-  <form>
-    <label htmlFor="name">Name</label>
-    <input type="text" name="name" />
-    <label htmlFor="age">Age</label>
-    <input type="number" name="age" />
-    <label htmlFor="course">Course</label>
-    <input type="text" name="course" />
-    <label htmlFor=""></label>
-  </form>;
+import './CreateStudentsForm.css';
+import { type StudentType } from '../../types/studentType';
+interface Props {
+  onAdd: (newStudent: StudentType) => void;
+}
+export const CreateStudentForm = ({ onAdd }: Props) => {
+  const formHandler = (formData: FormData) => {
+    const name = formData.get('name') as string;
+    const age = Number(formData.get('age'));
+    const course = formData.get('course') as string;
+    const isOnline: boolean = false;
+    const avatar = formData.get('avatar') as string;
+    const newStudent = {
+      id: Date.now(),
+      name,
+      age,
+      course,
+      isOnline,
+      avatar,
+    };
+    onAdd(newStudent);
+  };
+  return (
+    <form action={formHandler} className="form">
+      <label htmlFor="name">Name</label>
+      <input required type="text" name="name" />
+      <label htmlFor="age">Age</label>
+      <input required type="number" name="age" />
+      <label htmlFor="course">Course</label>
+      <input required type="text" name="course" />
+      <label htmlFor="avatar">image</label>
+      <input type="text" name="avatar" />
+      <button>Submit</button>
+    </form>
+  );
 };
